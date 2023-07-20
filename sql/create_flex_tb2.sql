0b009337e944aec02b52d22a04bcae5be9f84568
jim-hardtke <jim.hardtke@flexagon.com>
2023-07-20 07:21:39 -0500

    Update create_flex_tb2.sql

--- sql/create_flex_tb2.sql
+++ sql/create_flex_tb2.sql
@@ -1,2 +1,10 @@
-drop table sysadm.flex_tb2;
+BEGIN
+   EXECUTE IMMEDIATE 'drop table sysadm.flex_tb2';
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN
+         RAISE;
+      END IF;
+END;
+/
 create table sysadm.flex_tb2 (c1 varchar2(5 char));
